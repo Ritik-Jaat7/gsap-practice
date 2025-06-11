07b52a7b85dfcba3f54dc07ce03df4abc054f87b
Ritik <rvjaat25@gmail.com>
2025-06-11 09:52:01 +0530

    code merge

--- src/components/minimal/Furniture.jsx
+++ src/components/minimal/Furniture.jsx
@@ -16,14 +16,14 @@ const Furniture = () => {
         { src: "/assets/images/circle-lamp.webp", width: 483, height: 633, className: 'min-w-[483px] h-[633px] mt-[230px]' },
         { src: "/assets/images/book-light.webp", width: 448, height: 588, className: 'min-w-[448px] h-[588px] object-cover' },
         { src: "/assets/images/pot.webp", width: 356, height: 467, className: 'w-[356px] h-[467px] ml-[250px] ', wrapper: true },
-        { src: "/assets/images/office-light.webp", width: 356, height: 467, className: 'w-[356px] h-[467px] opacity-0' },
-        { src: "/assets/images/two-chairs.webp", width: 248, height: 325, className: 'w-[248px] h-[325px] opacity-0' },
+        { src: "/assets/images/office-light.webp", width: 356, height: 467, className: 'w-[356px] h-[467px] !opacity-0' },
+        { src: "/assets/images/two-chairs.webp", width: 248, height: 325, className: 'w-[248px] h-[325px] !opacity-0' },
         { src: "/assets/images/office-light.webp", width: 334, height: 438, className: 'w-[334px] h-[438px] mt-[100px]' },
         { src: "/assets/images/two-chairs.webp", width: 248, height: 325, className: 'w-[248px] h-[325px] mt-[100px]', wrapper: true },
         { src: "/assets/images/gold-biskuts.webp", width: 162, height: 212, className: 'w-[162px] h-[212px]', wrapper: true, },
         { src: "/assets/images/chair-table-laptop.webp", width: 420, height: 551, className: 'min-w-[420px] h-[551px] ', },
         { src: "/assets/images/less-more.webp", width: 334, height: 438, className: 'min-w-[334px] h-[438px] mt-[100px]', },
-        { src: "/assets/images/tech-5.webp", width: 248, height: 325, className: 'min-w-[248px] h-[325px] mt-[520px]', wrapper: true, },
+        { src: "/assets/images/tech-5.webp", width: 248, height: 325, className: 'min-w-[248px] h-[325px]', wrapper: true, },
         { src: "/assets/images/camera.webp", width: 162, height: 212, className: 'min-w-[162px] h-[212px] mt-20', },
         { src: "/assets/images/digital-watch.webp", width: 420, height: 551, className: 'min-w-[420px] h-[551px] mt-40', },
         { src: "/assets/images/cycle.webp", width: 420, height: 551, className: 'min-w-[420px] h-[551px]', },
@@ -31,31 +31,38 @@ const Furniture = () => {
     ]
 
     useEffect(() => {
-        gsap.set(".gridImages", { yPercent: 40 })
-
+        gsap.set(".gridImages", { yPercent: 30 })
+        gsap.to(".furnitureContainer", {
+            scrollTrigger: {
+                trigger: ".furnitureContainer",
+                start: "top 75%",
+                end: "+=200%",
+                pin: true,
+                scrub: 3,
+                markers: false,
+            }
+        })
         gsap.to(".gridImages", {
             yPercent: -100,
             scrollTrigger: {
                 trigger: ".furnitureContainer",
-                start: "top 70%",
-                end: "+=1000",
-                scrub: true,
-                pin: true,
-                markers: true,
+                start: "top 75%",
+                end: "+=200%",
+                scrub: 3,
+                markers: false,
             }
         })
         const boxes = gsap.utils.toArray(".slideImg")
-        boxes.forEach((img) => {
+        boxes.forEach((img, index) => {
             gsap.to(img, {
-                yPercent: -50,
+                yPercent: -30,
                 scrollTrigger: {
-                    trigger: ".gridImages",
-                    start: "top bottom",
-                    end: "+=100%",
-                    scrub: 2,
-                    markers: true,
-                    id: 'img slider'
-                },
+                    trigger: ".furnitureContainer",
+                    start: "top 80%",
+                    end: "+=200%",
+                    scrub: 3,
+                    markers: false,
+                }
             })
         })
     }, [])
